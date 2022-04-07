4f0b2165ebe2c038124ad145272054190c9f7697
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-07 16:42:14 +0530

    [Ronit] Added : Welcome Massage

---
+++ Day-14 Linked List/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList Program");
+            Console.ReadLine();
+        }
+    }
+}
