f54192a6de7fb8765ddbd4f478d8e6d807510510
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-07 16:48:03 +0530

    [Ronit] Added : Creating LinkedList Add numbers

--- Day-14 Linked List/Program.cs
+++ Day-14 Linked List/Program.cs
@@ -12,6 +12,14 @@ namespace Day_14_Linked_List
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LinkedList Program");
+            LinkedList<int> list = new LinkedList<int>(); // creating a object of in built LinkedList generic class 
+            list.AddFirst(56); // Calling method of Linked List generic class to add 56 at first
+            list.AddLast(30); // Calling method of Linked List generic class to add 30 at last
+            list.AddLast(70); // Calling method of Linked List generic class to add 70 at last
+            foreach (int element in list)
+            {
+                Console.WriteLine(element); // Printing all the element of linked list
+            }
             Console.ReadLine();
         }
     }
